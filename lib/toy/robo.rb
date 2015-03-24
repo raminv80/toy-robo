f4f8f3faf9fd2f6186300f07bb3619d79dfec42d
Ramin Vakilian <ramin@motionworks.com.my>
2015-03-24 20:49:23 +1030

    adding libraries to the gem

--- lib/toy/robo.rb
+++ lib/toy/robo.rb
@@ -1,4 +1,10 @@
 require "toy/robo/version"
+require "toy/robo/robot"
+require "toy/robo/table"
+require "toy/robo/transformation"
+require "toy/robo/commands"
+require "toy/robo/invoker"
+
 module Toy
   module Robo
   end
