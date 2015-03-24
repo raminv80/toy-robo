8015976ed3dde22cb9d5338863bc54a65026c2f8
Ramin Vakilian <ramin@motionworks.com.my>
2015-03-24 20:50:35 +1030

    rspec

--- spec/toy/robo_spec.rb
+++ spec/toy/robo_spec.rb
@@ -5,4 +5,31 @@ describe Toy::Robo do
     expect(Toy::Robo::VERSION).not_to be nil
   end
 
+  describe Toy::Robo::Simulator do
+    subject { Toy::Robo::Simulator.new }
+
+    before :each do
+      subject.execute('PLACE 0,0,NORTH')
+    end
+
+    it "with 'move' command" do
+      subject.execute('MOVE')
+      expect(subject.execute('REPORT')).to eq("Output: 0,1,NORTH")
+    end
+
+    it "with 'left' command" do
+      subject.execute('LEFT')
+      expect(subject.execute('REPORT')).to eq("Output: 0,0,WEST")
+    end
+
+    it "with 'right' command" do
+      subject.execute('RIGHT')
+      expect(subject.execute('REPORT')).to eq("Output: 0,0,EAST")
+    end
+
+    it "with 'place' command" do
+      subject.execute('PLACE 1,1,SOUTH')
+      expect(subject.execute('REPORT')).to eq("Output: 1,1,SOUTH")
+    end
+  end
 end
